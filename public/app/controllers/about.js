2bd7aa162f3112dbefbfa89916342418ce15e356
Zach Cannon <zcannon@dmp.com>
2017-04-12 18:23:28 -0500

    Made About Static

--- public/app/controllers/about.js
+++ public/app/controllers/about.js
@@ -1,6 +1,4 @@
 angular.module('app.aboutCtrl', [])
   .controller('aboutCtrl', ['$scope', '$sce', 'services', function ($scope, $sce, services) {
-    services.getAbout().then(function(data){ $scope.post = '' + data.data
-    $scope.post = $sce.trustAsHtml($scope.post)
-  }, function (error){ console.log(error) })
+    
   }]);
--- public/app/controllers/blog/blog.js
+++ public/app/controllers/blog/blog.js
@@ -1,11 +1,14 @@
 angular.module('app.blogHomeCtrl', [])
   .controller('blogHomeCtrl', ['$scope', '$sce', 'services', function ($scope, $sce, services) {
+    $scope.loading = true;
     $scope.toTrustedHTML = function( html ){
     return $sce.trustAsHtml( html );
     }
     services.getBlogs().then(function(data) {
       $scope.blogs = data.data;
+      $scope.loading = false;
     }, function (error) {
       console.log(error)
+      $scope.loading = false;
     })  
   }])
